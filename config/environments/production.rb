ece9700da9fdcfee1c574cd8e267fa10ba9b5c6e
vscode docker <zoxplers@gmail.com>
2023-03-04 02:50:59 +0000

    Heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "group6.zoxplers.com"
+  config.hosts << "group6-se2023.herokuapp.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
